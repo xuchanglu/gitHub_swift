00d692727069561c83be1c8adf5fcf5c2cc1144e
许常璐 <3138592536@qq.com>
2016-11-02 11:14:04 +0800

    第一次提交
    第一次提交

--- gitHub_swift/gitHub_swift/ViewController.swift
+++ gitHub_swift/gitHub_swift/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.red
+        view.addSubview(UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100)))
+        
     }
 
     override func didReceiveMemoryWarning() {
